d1e4546461c1e9edd902ef7e51d52625f6dfd146
Oskar-Ciebien <G00369579@gmit.ie>
2020-12-10 11:32:29 +0000

    Lab Upload

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -4,17 +4,22 @@ const app = express()
 const port = 4000
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
+const path = require('path');
 
 // CORS
-const cors = require('cors');
-app.use(cors());
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// const cors = require('cors');
+// app.use(cors());
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//     res.header("Access-Control-Allow-Headers",
+//         "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
+
+// Build - Static for Front-End
+app.use(express.static(path.join(__dirname, '../build')));
+app.use('/static', express.static(path.join(__dirname, 'build/static')));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -39,11 +44,11 @@ const movieSchema = new Schema({
 // Model
 const movieModel = mongoose.model('movie', movieSchema);
 
-// Home page
-app.get('/', (req, res) => {
-    // Main Code
-    res.send('Hello World!')
-})
+// // Home page
+// app.get('/', (req, res) => {
+//     // Main Code
+//     res.send('Hello World!')
+// })
 
 // /api/movies/:id
 app.get('/api/movies/:id', (req, res) => {
@@ -111,6 +116,11 @@ app.post('/api/movies', (req, res) => {
     })
 
     res.send('Data Received!')
+})
+
+// All other urls
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/../build/index.html'));
 })
 
 // Listen
